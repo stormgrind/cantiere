cc192a6ebc98ad6a8dd1d0a25f2adc31826d1269
Marek Goldmann <marek.goldmann@gmail.com>
2011-07-19 01:16:37 -0700

    Add Fedora 15 to the list.

--- lib/cantiere/defaults.rb
+++ lib/cantiere/defaults.rb
@@ -28,7 +28,7 @@ module Cantiere
   # here are global variables
   SUPPORTED_ARCHES = [ "i386", "x86_64" ]
   SUPPORTED_OSES = {
-          "fedora" => [ "14", "13", "12", "11", "rawhide" ],
+          "fedora" => [ "15", "14", "13", "12", "11", "rawhide" ],
           "rhel" => ["5"]
   }
 
